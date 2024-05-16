6c63b8d1c7076f3f95c79412063c2f6c4389b35e
Trolleye <petovorcak@gmail.com>
2024-05-16 19:14:25 +0200

    fix checkstyle v maine

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -12,43 +12,43 @@ import com.mygdx.game.units.Unit;
 import java.util.ArrayList;
 
 public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Player player;
-	ArrayList<Unit> UnitArrayList = new ArrayList<Unit>();
-	EnemySpawner enemySpawner;
-	private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private Player player;
+    private final ArrayList<Unit> unitArrayList = new ArrayList<Unit>();
+    private EnemySpawner enemySpawner;
+    private OrthographicCamera camera;
 
-	@Override
-	public void create () {
-		this.batch = new SpriteBatch();
-		this.player = new Player();
-		this.UnitArrayList.add(this.player);
-		this.enemySpawner = new EnemySpawner(this.player, this.UnitArrayList);
-		this.camera = new OrthographicCamera(1280, 720);
-		this.camera.setToOrtho(false);
-	}
+    @Override
+    public void create () {
+        this.batch = new SpriteBatch();
+        this.player = new Player();
+        this.unitArrayList.add(this.player);
+        this.enemySpawner = new EnemySpawner(this.player, this.unitArrayList);
+        this.camera = new OrthographicCamera(1280, 720);
+        this.camera.setToOrtho(false);
+    }
 
-	@Override
-	public void render () {
-		this.batch.begin();
-		this.camera.position.set(this.player.getPlayerPos().x, this.player.getPlayerPos().y, 0);
-		this.camera.update();
-		ScreenUtils.clear(0, 0, 0, 0);
-	if (Gdx.input.isKeyPressed(Input.Keys.P)) {
-				this.enemySpawner.spawn();
-	}
-		if (Gdx.input.isKeyPressed(Input.Keys.O)) {
-			this.UnitArrayList.clear();
-		}
-		for (Unit unit : this.UnitArrayList) {
-			unit.vykresli(this.batch);
-		}
-		this.batch.setProjectionMatrix(this.camera.combined);
-		this.batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		this.batch.dispose();
-	}
+    @Override
+    public void render () {
+        this.batch.begin();
+        this.camera.position.set(this.player.getPlayerPos().x, this.player.getPlayerPos().y, 0);
+        this.camera.update();
+        ScreenUtils.clear(0, 0, 0, 0);
+        if (Gdx.input.isKeyPressed(Input.Keys.P)) {
+            this.enemySpawner.spawn();
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.O)) {
+            this.unitArrayList.clear();
+        }
+        for (Unit unit : this.unitArrayList) {
+            unit.vykresli(this.batch);
+        }
+        this.batch.setProjectionMatrix(this.camera.combined);
+        this.batch.end();
+    }
+
+    @Override
+    public void dispose () {
+        this.batch.dispose();
+    }
 }
--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -3,12 +3,12 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setResizable(false);
-		config.setWindowedMode(1280, 720);
-		config.setForegroundFPS(60);
-		config.setTitle("Hero Survivors");
-		new Lwjgl3Application(new Main(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setResizable(false);
+        config.setWindowedMode(1280, 720);
+        config.setForegroundFPS(60);
+        config.setTitle("Hero Survivors");
+        new Lwjgl3Application(new Main(), config);
+    }
 }
